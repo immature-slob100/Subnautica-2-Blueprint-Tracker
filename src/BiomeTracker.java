e9cf17711d46db19bda6ce64f2ddcac1c31c5ec4
WaveGumshoePropagate <g2785034@gmail.com>
2026-06-13 08:56:53 +0200

    Create BiomeTracker.java

---
+++ src/BiomeTracker.java
@@ -0,0 +1,12 @@
+public class BiomeTracker {
+
+    private int discoveredBiomes;
+
+    public int getDiscoveredBiomes() {
+        return discoveredBiomes;
+    }
+
+    public void setDiscoveredBiomes(int value) {
+        discoveredBiomes = value;
+    }
+}
